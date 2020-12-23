e560949b541a6bd0a064d97ea1e656c63dcc5502
Ken Taketani <takekanik5@gmail.com>
2020-12-23 21:04:44 +0900

    update

--- Editor/ComonySdkMenu.cs
+++ Editor/ComonySdkMenu.cs
@@ -1,8 +1,14 @@
 using UnityEditor;
 using UnityEngine;
 
+[InitializeOnLoad]
 public class ComonySdkMenu : MonoBehaviour
 {
+    static ComonySdkMenu()
+    {
+        Debug.Log("Update");
+    }
+
     [MenuItem("ComonySDK/Upload Window")]
     static void ShowUploadWindow()
     {
